5795d042fd8b7e888fb7b8712719f3c10dd02bd1
Zabbist <55331386+Zabbist@users.noreply.github.com>
2023-01-03 18:15:59 +0000

    Update negative.js

--- negative.js
+++ negative.js
@@ -18,3 +18,44 @@ function removeDuplicates() {
   // Update the value of the input element
   inputElement.value = inputValue;
 }
+
+function saveToJSON() {
+  // Get the input element and its value
+  const inputElement = document.getElementById("input");
+  const inputValue = inputElement.value;
+
+  // Split the input value into an array of words or phrases
+  const inputArray = inputValue.split(",");
+
+  // Remove any leading or trailing whitespace from the array elements
+  const cleanInputArray = inputArray.map(str => str.trim());
+
+  // Convert the array to a JSON object
+  const data = {
+    words: cleanInputArray
+  };
+
+  // Stringify the JSON object
+  const json = JSON.stringify(data);
+
+  // Create a Blob object with the JSON string as its content
+  const blob = new Blob([json], {
+    type: "application/json"
+  });
+
+  // Create a link element
+  const a = document.createElement("a");
+
+  // Set the link's download attribute to a file name and add the Blob object as its href
+  a.download = "words.json";
+  a.href = URL.createObjectURL(blob);
+
+  // Append the link element to the document
+  document.body.appendChild(a);
+
+  // Click the link to download the JSON file
+  a.click();
+
+  // Remove the link element from the document
+  document.body.removeChild(a);
+}
