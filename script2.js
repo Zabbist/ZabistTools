717f4ed9e2d7ec7644dd32bcbb895c82091dff75
Zabbist <55331386+Zabbist@users.noreply.github.com>
2023-01-10 20:47:05 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -174,6 +174,19 @@ function copyToClipboard() {
   });
 }
 
+function copyNegativeToClipboard(){
+  const text = "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, signature, cut off, draft, duplicate, coppy, multi, two faces, two, disfigured, kitsch, ugly, oversaturated, grain, low-res";
+    navigator.clipboard.writeText(text).then(function() {
+    /* clipboard successfully set */
+    displayCopyMessage();
+  }, function() {
+    /* clipboard write failed */
+    console.log("Clipboard write failed");
+  });
+}
+
+
+
 function displayCopyMessage() {
   var copiedText = document.createElement("div");
   copiedText.style.cssText = `
