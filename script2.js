ecabd584ef911e974ccd200efde289ba488faa35
Zabbist <55331386+Zabbist@users.noreply.github.com>
2023-01-10 20:09:41 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,7 +4,30 @@ function copyToClipboard() {
   const randomColor = colorList[Math.floor(Math.random() * colorList.length)];
   navigator.clipboard.writeText(randomColor).then(function() {
     /* clipboard successfully set */
+    displayCopyMessage();
   }, function() {
     /* clipboard write failed */
+    console.log("Clipboard write failed");
   });
+}
+
+function displayCopyMessage() {
+  var copiedText = document.createElement("div");
+  copiedText.style.cssText = `
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    background-color: lightgray;
+    padding: 1em;
+  `;
+  copiedText.textContent = "Copied to clipboard!";
+  document.body.appendChild(copiedText);
+
+  setTimeout(() => {
+    copiedText.remove();
+  }, 2000);
 }
